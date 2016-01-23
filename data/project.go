698f7439198d0e76e532cdc32acc0615b354f938
nuronial_block <ahnaf@furqansoftware.com>
2016-01-23 19:39:34 +0600

    Implement GetProjectin Project

--- data/project.go
+++ data/project.go
@@ -3,6 +3,7 @@ package data
 import (
 	"time"
 
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -13,4 +14,16 @@ type Project struct {
 	MemberIds  []bson.ObjectId `bson:"member_ids"`
 	CreatedAt  time.Time       `bson:"created_at"`
 	ModifiedAt time.Time       `bson:"modified_at"`
+}
+
+func GetProject(id bson.ObjectId) (*Project, error) {
+	pro := Project{}
+	err := sess.DB("").C(projectC).FindId(id).One(&pro)
+	if err == mgo.ErrNotFound {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &pro, nil
 }
