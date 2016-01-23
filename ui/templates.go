7f3b70741f769aaf21e35b2f9adc0ad3dbc7dbfb
Syrus <msyrus@gmail.com>
2016-01-23 14:39:51 +0600

    Implement ServeHTMLTemplate

--- ui/templates.go
+++ ui/templates.go
@@ -1,6 +1,20 @@
 package ui
 
-import "text/template"
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"text/template"
+)
+
+func ServeHTMLTemplate(w http.ResponseWriter, r *http.Request, tpl *template.Template, data interface{}) {
+	buf := bytes.Buffer{}
+	err := tpl.Execute(&buf, data)
+	catch(r, err)
+	w.Header().Set("Content-Type", "text/html")
+	_, err = io.Copy(w, &buf)
+	catch(r, err)
+}
 
 var (
 	tplLayout = template.Must(template.New("layout.html").ParseFiles("ui/templates/layout.html", "ui/templates/common.html"))
