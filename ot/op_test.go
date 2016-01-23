748314ff070a1865959cf460a61411d062a5aa95
Mahmud Ridwan <m@hjr265.me>
2016-01-23 15:45:20 +0600

    Implement retain-retain transform test

--- ot/op_test.go
+++ ot/op_test.go
@@ -147,3 +147,42 @@ func TestInsertOpComposeDelete(t *testing.T) {
 		}
 	}
 }
+
+func TestRetainOpTransformRetain(t *testing.T) {
+	cases := []struct {
+		ret1 RetainOp
+		ret2 RetainOp
+		exp  [4]Op
+	}{
+		{
+			ret1: 6,
+			ret2: 6,
+			exp:  [4]Op{RetainOp(6), RetainOp(6), Noop, Noop},
+		},
+		{
+			ret1: 6,
+			ret2: 3,
+			exp:  [4]Op{RetainOp(3), RetainOp(3), RetainOp(3), Noop},
+		},
+		{
+			ret1: 6,
+			ret2: 9,
+			exp:  [4]Op{RetainOp(6), RetainOp(6), Noop, RetainOp(3)},
+		},
+	}
+	for i, c := range cases {
+		x, y, p, q := c.ret1.TransformRetain(c.ret2)
+		if x != c.exp[0] {
+			t.Fatalf("%d: expected x == %#v, got %#v", i, c.exp[0], x)
+		}
+		if y != c.exp[1] {
+			t.Fatalf("%d: expected y == %#v, got %#v", i, c.exp[1], y)
+		}
+		if p != c.exp[2] {
+			t.Fatalf("%d: expected p == %#v, got %#v", i, c.exp[1], p)
+		}
+		if q != c.exp[3] {
+			t.Fatalf("%d: expected q == %#v, got %#v", i, c.exp[2], q)
+		}
+	}
+}
