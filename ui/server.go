858731fc9ceab9cb4daf3223c685cfa486a36577
Syrus <msyrus@gmail.com>
2016-01-23 11:21:42 +0600

    Implement Session

--- ui/server.go
+++ ui/server.go
@@ -2,17 +2,44 @@ package ui
 
 import (
 	"net/http"
+	"os"
 
+	"gopkg.in/mgo.v2/bson"
+
+	"github.com/gophergala2016/papyrus/data"
+	"github.com/gorilla/context"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 )
 
 var Router = mux.NewRouter()
+
+var store = sessions.NewCookieStore([]byte(os.Getenv("SECRET")))
 
 type Server struct {
 	Router *mux.Router
 }
 
 func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	sess, err := store.Get(r, "s")
+	if err != nil {
+		ServeInternalServerError(w, r)
+		return
+	}
+
+	accID, ok := sess.Values["accountID"].(string)
+	if ok {
+		if !bson.IsObjectIdHex(accID) {
+			ServeBadRequest(w, r)
+			return
+		}
+		acc, err := data.GetAccount(bson.ObjectIdHex(accID))
+		if err != nil {
+			ServeInternalServerError(w, r)
+			return
+		}
+		context.Set(r, "account", acc)
+	}
 
 	s.Router.ServeHTTP(w, r)
 }
