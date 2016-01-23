eb9165a0e0b70b30b54d1d35d467cec099d3feed
nuronial_block <ahnaf@furqansoftware.com>
2016-01-23 13:05:19 +0600

    Implement NewAccountEmail

--- data/accountemail.go
+++ data/accountemail.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+)
 
 type AccountEmail struct {
 	Address     string `bson:"address"`
@@ -11,4 +15,15 @@ type AccountEmail struct {
 	Verified   bool      `bson:"verified"`
 	VerifiedAt time.Time `bson:"verified_at"`
 	Token      string    `bson:"token"`
+}
+
+func NewAccountEmail(addr string) (AccountEmail, error) {
+	norm, err := govalidator.NormalizeEmail(addr)
+	if err != nil {
+		return AccountEmail{}, err
+	}
+	return AccountEmail{
+		Address:     addr,
+		AddressNorm: norm,
+	}, nil
 }
