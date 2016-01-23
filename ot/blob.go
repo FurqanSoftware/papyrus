6743c795ecb818801137990010ec6b21f5b1b971
Mahmud Ridwan <m@hjr265.me>
2016-01-23 16:28:18 +0600

    Implement blob with ops apply method

---
+++ ot/blob.go
@@ -0,0 +1,42 @@
+package ot
+
+import (
+	"bytes"
+	"errors"
+)
+
+var (
+	ErrBadSpan = errors.New("ot: base span doesn't match blob length")
+)
+
+type Blob []byte
+
+func (b *Blob) Apply(u Ops) error {
+	if u.SpanBase() != len(*b) {
+		return ErrBadSpan
+	}
+
+	z := bytes.Buffer{}
+
+	d := *b
+	i := 0
+	for _, o := range u {
+		switch o := o.(type) {
+		case RetainOp:
+			z.Write(d[i : i+int(o)])
+			i += int(o)
+
+		case InsertOp:
+			z.Write([]byte(o))
+
+		case DeleteOp:
+			i += int(o)
+		}
+	}
+	if i != len(d) {
+		return ErrTooShort
+	}
+
+	*b = z.Bytes()
+	return nil
+}
