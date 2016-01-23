506b795c2f8f0a40b8644f866dad6563e2e42a8d
Syrus <msyrus@gmail.com>
2016-01-23 11:13:26 +0600

    Implement GetAccount

--- data/account.go
+++ data/account.go
@@ -3,6 +3,7 @@ package data
 import (
 	"time"
 
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -14,4 +15,17 @@ type Account struct {
 
 	CreatedAt  time.Time `bson:"created_at"`
 	ModifiedAt time.Time `bson:"modified_at"`
+}
+
+func GetAccount(id bson.ObjectId) (*Account, error) {
+	acc := Account{}
+
+	err := sess.DB("").C(accountC).FindId(id).One(&acc)
+	if err == mgo.ErrNotFound {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &acc, nil
 }
