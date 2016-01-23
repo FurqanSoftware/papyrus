c0825ad0511bedbbc8b3b5932354b4f0770e4a9c
Mahmud Ridwan <m@hjr265.me>
2016-01-23 13:15:41 +0600

    Implement compose interfaces

--- ot/op.go
+++ ot/op.go
@@ -16,30 +16,83 @@ type Op interface {
 
 type RetainOp int
 
-func (o RetainOp) Type() OpType {
+func (p RetainOp) Type() OpType {
 	return OpRetain
 }
 
-func (o RetainOp) Span() int {
-	return int(o)
+func (p RetainOp) Span() int {
+	return int(p)
 }
+
+func (p RetainOp) ComposeRetain(q RetainOp) (Op, Op, Op) {
+	switch {
+	case p > q:
+		return q, p - q, Noop
+
+	default:
+		return p, Noop, q - p
+	}
+}
+
+func (p RetainOp) ComposeDelete(q DeleteOp) (Op, Op, Op) {
+	switch {
+	case int(p) < int(q):
+		return DeleteOp(p), Noop, q - DeleteOp(p)
+
+	default:
+		return q, p - RetainOp(q), Noop
+	}
+}
+
+var Noop = RetainOp(0)
 
 type InsertOp string
 
-func (o InsertOp) Type() OpType {
+func (p InsertOp) Type() OpType {
 	return OpInsert
 }
 
-func (o InsertOp) Span() int {
-	return len(o)
+func (p InsertOp) Span() int {
+	return len(p)
+}
+
+func (p InsertOp) ComposeRetain(q RetainOp) (Op, Op, Op) {
+	switch {
+	case len(p) > int(q):
+		return p[:int(q)], p[int(q):], Noop
+
+	default:
+		return p, Noop, q - RetainOp(len(p))
+	}
+}
+
+func (p InsertOp) ComposeDelete(q DeleteOp) (Op, Op, Op) {
+	switch {
+	case len(p) > int(q):
+		return Noop, p[int(q):], Noop
+
+	case len(p) < int(q):
+		return Noop, Noop, q - DeleteOp(len(p))
+
+	default:
+		return Noop, Noop, Noop
+	}
 }
 
 type DeleteOp int
 
-func (o DeleteOp) Type() OpType {
+func (p DeleteOp) Type() OpType {
 	return OpDelete
 }
 
-func (o DeleteOp) Span() int {
-	return int(o)
+func (p DeleteOp) Span() int {
+	return int(p)
+}
+
+type RetainComposer interface {
+	ComposeRetain(q RetainOp) (Op, Op, Op)
+}
+
+type DeleteComposer interface {
+	ComposeDelete(q DeleteOp) (Op, Op, Op)
 }
---
+++ ot/op_test.go
@@ -0,0 +1,149 @@
+package ot
+
+import (
+	"testing"
+)
+
+func TestRetainOpComposeRetain(t *testing.T) {
+	cases := []struct {
+		ret1 RetainOp
+		ret2 RetainOp
+		exp  [3]Op
+	}{
+		{
+			ret1: 6,
+			ret2: 6,
+			exp:  [3]Op{RetainOp(6), Noop, Noop},
+		},
+		{
+			ret1: 6,
+			ret2: 3,
+			exp:  [3]Op{RetainOp(3), RetainOp(3), Noop},
+		},
+		{
+			ret1: 6,
+			ret2: 9,
+			exp:  [3]Op{RetainOp(6), Noop, RetainOp(3)},
+		},
+	}
+	for i, c := range cases {
+		x, p, q := c.ret1.ComposeRetain(c.ret2)
+		if x != c.exp[0] {
+			t.Fatalf("%d: expected x == %#v, got %#v", i, c.exp[0], x)
+		}
+		if p != c.exp[1] {
+			t.Fatalf("%d: expected p == %#v, got %#v", i, c.exp[1], p)
+		}
+		if q != c.exp[2] {
+			t.Fatalf("%d: expected q == %#v, got %#v", i, c.exp[2], q)
+		}
+	}
+}
+
+func TestInsertOpComposeRetain(t *testing.T) {
+	cases := []struct {
+		ins InsertOp
+		ret RetainOp
+		exp [3]Op
+	}{
+		{
+			ins: "foobar",
+			ret: 6,
+			exp: [3]Op{InsertOp("foobar"), Noop, Noop},
+		},
+		{
+			ins: "foobar",
+			ret: 3,
+			exp: [3]Op{InsertOp("foo"), InsertOp("bar"), Noop},
+		},
+		{
+			ins: "foobar",
+			ret: 9,
+			exp: [3]Op{InsertOp("foobar"), Noop, RetainOp(3)},
+		},
+	}
+	for i, c := range cases {
+		x, p, q := c.ins.ComposeRetain(c.ret)
+		if x != c.exp[0] {
+			t.Fatalf("%d: expected x == %#v, got %#v", i, c.exp[0], x)
+		}
+		if p != c.exp[1] {
+			t.Fatalf("%d: expected p == %#v, got %#v", i, c.exp[1], p)
+		}
+		if q != c.exp[2] {
+			t.Fatalf("%d: expected q == %#v, got %#v", i, c.exp[2], q)
+		}
+	}
+}
+
+func TestRetainOpComposeDelete(t *testing.T) {
+	cases := []struct {
+		ret RetainOp
+		del DeleteOp
+		exp [3]Op
+	}{
+		{
+			ret: 6,
+			del: 6,
+			exp: [3]Op{DeleteOp(6), Noop, Noop},
+		},
+		{
+			ret: 6,
+			del: 3,
+			exp: [3]Op{DeleteOp(3), RetainOp(3), Noop},
+		},
+		{
+			ret: 6,
+			del: 9,
+			exp: [3]Op{DeleteOp(6), Noop, DeleteOp(3)},
+		},
+	}
+	for i, c := range cases {
+		x, p, q := c.ret.ComposeDelete(c.del)
+		if x != c.exp[0] {
+			t.Fatalf("%d: expected x == %#v, got %#v", i, c.exp[0], x)
+		}
+		if p != c.exp[1] {
+			t.Fatalf("%d: expected p == %#v, got %#v", i, c.exp[1], p)
+		}
+		if q != c.exp[2] {
+			t.Fatalf("%d: expected q == %#v, got %#v", i, c.exp[2], q)
+		}
+	}
+}
+
+func TestInsertOpComposeDelete(t *testing.T) {
+	cases := []struct {
+		ins InsertOp
+		del DeleteOp
+		exp [3]Op
+	}{
+		{
+			ins: "foobar",
+			del: 6,
+			exp: [3]Op{Noop, Noop, Noop},
+		},
+		{
+			ins: "foobar",
+			del: 3,
+			exp: [3]Op{Noop, InsertOp("bar"), Noop},
+		},
+		{
+			ins: "foobar",
+			del: 9,
+			exp: [3]Op{Noop, Noop, DeleteOp(3)},
+		},
+	}
+	for i, c := range cases {
+		x, p, q := c.ins.ComposeDelete(c.del)
+		if x != c.exp[0] {
+			t.Fatalf("%d: expected x == %#v, got %#v", i, c.exp[0], x)
+		}
+		if p != c.exp[1] {
+			t.Fatalf("%d: expected p == %#v, got %#v", i, c.exp[1], p)
+		}
+		if q != c.exp[2] {
+			t.Fatalf("%d: expected q == %#v, got %#v", i, c.exp[2], q)
+		}
+	}
+}
