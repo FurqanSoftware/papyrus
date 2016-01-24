6c9ececbf638ce3dc95c920a7ef29f40734119b1
Syrus <msyrus@gmail.com>
2016-01-24 15:09:28 +0600

    Update indexes

--- data/index.go
+++ data/index.go
@@ -9,6 +9,34 @@ var indexes = map[string][]mgo.Index{
 			Unique: true,
 		},
 	},
+	documentC: {
+		{
+			Key:    []string{"short_id"},
+			Unique: true,
+		},
+		{
+			Key: []string{"project_id"},
+		},
+	},
+	memberC: {
+		{
+			Key:    []string{"project_id", "account_id"},
+			Unique: true,
+		},
+		{
+			Key: []string{"account_id", "organization_id"},
+		},
+	},
+	organizationC: {
+		{
+			Key: []string{"owner_id"},
+		},
+	},
+	projectC: {
+		{
+			Key: []string{"organization_id"},
+		},
+	},
 }
 
 func MakeIndexes() error {
